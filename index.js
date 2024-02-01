2f375e0286c527720d8a4c8ef30f2a1a3cd9414d
Mm9450 <manas1999mishra@gmail.com>
2024-02-01 22:53:10 +0530

    Javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+//OBJECTS
+const mySym = Symbol("key1")
+const JSUser = {
+    [mySym]: "myKey1",
+    name: "Manas",
+    age: 18,
+    location: "Lucknow",
+    Email: "abcd@gmail.com",
+    isLoggedin : false
+}
+
+// console.log(JSUser.location);
+// console.log(JSUser["Email"]);
+// console.log(JSUser[mySym]);
+
+// console.log(JSUser);
+
