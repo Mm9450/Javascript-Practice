1a65378be7e4399a6fda4f9cae37daf2fe0afcca
Mm9450 <manas1999mishra@gmail.com>
2024-02-04 22:29:45 +0530

    Javascript

--- index.js
+++ index.js
@@ -60,3 +60,49 @@ const obj3 ={...obj1, ...obj2}
 // console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('id'));
+// <+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>
+// Functions in Javascripts
+
+function sayMyName(){
+    console.log("Manas");
+}
+//sayMyName()
+
+function addTwoNumbers(num1,num2){
+    console.log(num1 + num2);
+}
+//addTwoNumbers(8,9)
+
+function addTwoNumbers(num1,num2){
+    let result = num1 + num2
+    return result
+}
+// const result = addTwoNumbers(3,5)
+// console.log("Result: ", result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+//console.log(loginUserMessage("Rahul"));
+
+function calculateCartPrice(...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200,500,400,600));
+
+const user = {
+    username: "Manas",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and is ${anyObject.price}`)
+}
+//handleObject(user)
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray));
